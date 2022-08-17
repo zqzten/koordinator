d51afe98a3bc074339f15d25ecef4c31dba627fc
佑祎 <zzw261520@alibaba-inc.com>
2022-08-17 19:09:11 +0800

    ack: add update reclaimed resource plugin

---
+++ pkg/slo-controller/config/ack_reclaimed_resource_config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"k8s.io/utils/pointer"
+)
+
+const (
+	ReclaimedResExtKey = "reclaimedResource"
+)
+
+func init() {
+	RegisterDefaultColocationExtension(ReclaimedResExtKey, defaultReclaimedResourceConfig)
+
+}
+
+type ReclaimedResourceConfig struct {
+	NodeUpdate *bool `json:"nodeUpdate,omitempty"`
+}
+
+var (
+	defaultReclaimedResourceConfig = &ReclaimedResourceConfig{
+		NodeUpdate: pointer.BoolPtr(true),
+	}
+)
+
+func ParseReclaimedResourceConfig(strategy *ColocationStrategy) (*ReclaimedResourceConfig, error) {
+	if strategy == nil || strategy.Extensions == nil {
+		return nil, nil
+	}
+	cfgIf, exist := strategy.Extensions[ReclaimedResExtKey]
+	if !exist {
+		return defaultReclaimedResourceConfig, nil
+	}
+
+	cfgStr, err := json.Marshal(cfgIf)
+	if err != nil {
+		return nil, fmt.Errorf("ReclaimedResourceConfig interface json marshal failed")
+	}
+
+	cfg := &ReclaimedResourceConfig{}
+	if err := json.Unmarshal(cfgStr, cfg); err != nil {
+		return nil, fmt.Errorf("ReclaimedResourceConfig json unmarshal convert failed")
+	}
+	return cfg, nil
+}
