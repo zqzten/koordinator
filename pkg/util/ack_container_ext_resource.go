a0d71ecc097f7d5624e90f9cdee90e058ad46bb1
佑祎 <zzw261520@alibaba-inc.com>
2022-08-17 21:02:44 +0800

    ack: support container reclaimed resource calculate

--- pkg/util/ack_container_ext_resource.go
+++ pkg/util/ack_container_ext_resource.go
@@ -1,22 +1,5 @@
-//go:build github
-// +build github
-
-
-/*
-Copyright 2022 The Koordinator Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+//go:build !github
+// +build !github
 
 package util
 
@@ -24,10 +7,15 @@ import (
 	corev1 "k8s.io/api/core/v1"
 
 	"github.com/koordinator-sh/koordinator/apis/extension"
+
+	uniext "gitlab.alibaba-inc.com/cos/unified-resource-api/apis/extension"
 )
 
 func GetContainerBatchMilliCPURequest(c *corev1.Container) int64 {
 	if cpuRequest, ok := c.Resources.Requests[extension.BatchCPU]; ok {
+		return cpuRequest.Value()
+	}
+	if cpuRequest, ok := c.Resources.Requests[uniext.AlibabaCloudReclaimedCPU]; ok {
 		return cpuRequest.Value()
 	}
 	return -1
@@ -37,18 +25,27 @@ func GetContainerBatchMilliCPULimit(c *corev1.Container) int64 {
 	if cpuLimit, ok := c.Resources.Limits[extension.BatchCPU]; ok {
 		return cpuLimit.Value()
 	}
+	if cpuLimit, ok := c.Resources.Limits[uniext.AlibabaCloudReclaimedCPU]; ok {
+		return cpuLimit.Value()
+	}
 	return -1
 }
 
 func GetContainerBatchMemoryByteRequest(c *corev1.Container) int64 {
-	if memLimit, ok := c.Resources.Requests[extension.BatchMemory]; ok {
-		return memLimit.Value()
+	if memRequest, ok := c.Resources.Requests[extension.BatchMemory]; ok {
+		return memRequest.Value()
+	}
+	if memRequest, ok := c.Resources.Requests[uniext.AlibabaCloudReclaimedMemory]; ok {
+		return memRequest.Value()
 	}
 	return -1
 }
 
 func GetContainerBatchMemoryByteLimit(c *corev1.Container) int64 {
 	if memLimit, ok := c.Resources.Limits[extension.BatchMemory]; ok {
+		return memLimit.Value()
+	}
+	if memLimit, ok := c.Resources.Limits[uniext.AlibabaCloudReclaimedMemory]; ok {
 		return memLimit.Value()
 	}
 	return -1
