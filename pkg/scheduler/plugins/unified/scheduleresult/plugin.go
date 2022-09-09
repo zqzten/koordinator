9c87d46c5c016f0c4cd51b6d7ff6808c20d0fead
wangjianyu <zmsjianyu@gmail.com>
2022-09-09 14:35:57 +0800

    koord-scheduler: add schedule result in prebind
    Signed-off-by: wangjianyu <zmsjianyu@gmail.com>

---
+++ pkg/scheduler/plugins/unified/scheduleresult/plugin.go
@@ -0,0 +1,96 @@
+/*
+Copyright 2022 The Koordinator Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package scheduleresult
+
+import (
+	"context"
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	apimachinerytypes "k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/util/retry"
+	"k8s.io/klog/v2"
+	"k8s.io/kubernetes/pkg/scheduler/framework"
+
+	extunified "github.com/koordinator-sh/koordinator/apis/extension/unified"
+	"github.com/koordinator-sh/koordinator/pkg/util"
+)
+
+var (
+	_ framework.PreBindPlugin = &Plugin{}
+)
+
+const (
+	// Name is the name of the plugin used in the plugin registry and configurations.
+	Name = "UnifiedScheduleResult"
+)
+
+type Plugin struct {
+	handle framework.Handle
+}
+
+func New(args runtime.Object, handle framework.Handle) (framework.Plugin, error) {
+	return &Plugin{
+		handle: handle,
+	}, nil
+}
+
+func (p *Plugin) Name() string { return Name }
+
+func (p *Plugin) PreBind(ctx context.Context, cycleState *framework.CycleState, pod *corev1.Pod, nodeName string) *framework.Status {
+	podOriginal := pod
+	pod = pod.DeepCopy()
+	if pod.Labels == nil {
+		pod.Labels = make(map[string]string)
+	}
+	pod.Labels[extunified.K8sLabelScheduleNodeName] = nodeName
+	if pod.Annotations == nil {
+		pod.Annotations = make(map[string]string)
+	}
+	updateTime := time.Now().In(time.FixedZone("CST", 8*3600)).Format(time.RFC3339Nano)
+	pod.Annotations[extunified.AnnotationSchedulerUpdateTime] = updateTime
+	if pod.Spec.NodeName == "" {
+		pod.Annotations[extunified.AnnotationSchedulerBindTime] = updateTime
+	}
+	patchBytes, err := util.GeneratePodPatch(podOriginal, pod)
+	if err != nil {
+		return framework.NewStatus(framework.Error, err.Error())
+	}
+	if string(patchBytes) == "{}" {
+		return nil
+	}
+	err = retry.OnError(
+		retry.DefaultRetry,
+		errors.IsTooManyRequests,
+		func() error {
+			_, err := p.handle.ClientSet().CoreV1().Pods(pod.Namespace).
+				Patch(ctx, pod.Name, apimachinerytypes.StrategicMergePatchType, patchBytes, metav1.PatchOptions{})
+			if err != nil {
+				klog.Error("Failed to patch Pod %s/%s, patch: %v, err: %v", pod.Namespace, pod.Name, string(patchBytes), err)
+			}
+			return err
+		})
+	if err != nil {
+		return framework.NewStatus(framework.Error, err.Error())
+	}
+
+	klog.V(4).Infof("Successfully patch Pod %s/%s, patch: %v", pod.Namespace, pod.Name, string(patchBytes))
+	return nil
+}
---
+++ pkg/scheduler/plugins/unified/scheduleresult/plugin_test.go
@@ -0,0 +1,166 @@
+package scheduleresult
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	apiruntime "k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/informers"
+	kubefake "k8s.io/client-go/kubernetes/fake"
+	scheduledconfig "k8s.io/kubernetes/pkg/scheduler/apis/config"
+	"k8s.io/kubernetes/pkg/scheduler/framework"
+	"k8s.io/kubernetes/pkg/scheduler/framework/plugins/defaultbinder"
+	"k8s.io/kubernetes/pkg/scheduler/framework/plugins/queuesort"
+	"k8s.io/kubernetes/pkg/scheduler/framework/runtime"
+	schedulertesting "k8s.io/kubernetes/pkg/scheduler/testing"
+
+	extunified "github.com/koordinator-sh/koordinator/apis/extension/unified"
+)
+
+var _ framework.SharedLister = &testSharedLister{}
+
+type testSharedLister struct {
+	nodes       []*corev1.Node
+	nodeInfos   []*framework.NodeInfo
+	nodeInfoMap map[string]*framework.NodeInfo
+}
+
+func newTestSharedLister(pods []*corev1.Pod, nodes []*corev1.Node) *testSharedLister {
+	nodeInfoMap := make(map[string]*framework.NodeInfo)
+	nodeInfos := make([]*framework.NodeInfo, 0)
+	for _, pod := range pods {
+		nodeName := pod.Spec.NodeName
+		if _, ok := nodeInfoMap[nodeName]; !ok {
+			nodeInfoMap[nodeName] = framework.NewNodeInfo()
+		}
+		nodeInfoMap[nodeName].AddPod(pod)
+	}
+	for _, node := range nodes {
+		if _, ok := nodeInfoMap[node.Name]; !ok {
+			nodeInfoMap[node.Name] = framework.NewNodeInfo()
+		}
+		nodeInfoMap[node.Name].SetNode(node)
+	}
+
+	for _, v := range nodeInfoMap {
+		nodeInfos = append(nodeInfos, v)
+	}
+
+	return &testSharedLister{
+		nodes:       nodes,
+		nodeInfos:   nodeInfos,
+		nodeInfoMap: nodeInfoMap,
+	}
+}
+
+func (f *testSharedLister) NodeInfos() framework.NodeInfoLister {
+	return f
+}
+
+func (f *testSharedLister) List() ([]*framework.NodeInfo, error) {
+	return f.nodeInfos, nil
+}
+
+func (f *testSharedLister) HavePodsWithAffinityList() ([]*framework.NodeInfo, error) {
+	return nil, nil
+}
+
+func (f *testSharedLister) HavePodsWithRequiredAntiAffinityList() ([]*framework.NodeInfo, error) {
+	return nil, nil
+}
+
+func (f *testSharedLister) Get(nodeName string) (*framework.NodeInfo, error) {
+	return f.nodeInfoMap[nodeName], nil
+}
+
+type pluginTestSuit struct {
+	framework.Handle
+	proxyNew runtime.PluginFactory
+	args     apiruntime.Object
+}
+
+func newPluginTestSuit(t *testing.T, nodes []*corev1.Node) *pluginTestSuit {
+	pluginConfig := scheduledconfig.PluginConfig{
+		Name: Name,
+		Args: nil,
+	}
+
+	registeredPlugins := []schedulertesting.RegisterPluginFunc{
+		func(reg *runtime.Registry, profile *scheduledconfig.KubeSchedulerProfile) {
+			profile.PluginConfig = []scheduledconfig.PluginConfig{
+				pluginConfig,
+			}
+		},
+		schedulertesting.RegisterBindPlugin(defaultbinder.Name, defaultbinder.New),
+		schedulertesting.RegisterQueueSortPlugin(queuesort.Name, queuesort.New),
+		schedulertesting.RegisterPreBindPlugin(Name, New),
+	}
+
+	cs := kubefake.NewSimpleClientset()
+	informerFactory := informers.NewSharedInformerFactory(cs, 0)
+	snapshot := newTestSharedLister(nil, nodes)
+	fh, err := schedulertesting.NewFramework(
+		registeredPlugins,
+		"koord-scheduler",
+		runtime.WithClientSet(cs),
+		runtime.WithInformerFactory(informerFactory),
+		runtime.WithSnapshotSharedLister(snapshot),
+	)
+	assert.Nil(t, err)
+	return &pluginTestSuit{
+		Handle:   fh,
+		proxyNew: New,
+		args:     nil,
+	}
+}
+
+func (p *pluginTestSuit) start() {
+	ctx := context.TODO()
+	p.Handle.SharedInformerFactory().Start(ctx.Done())
+	p.Handle.SharedInformerFactory().WaitForCacheSync(ctx.Done())
+}
+
+func TestPlugin_PreBind(t *testing.T) {
+	tests := []struct {
+		name     string
+		pod      *corev1.Pod
+		nodeName string
+		want     *framework.Status
+	}{
+		{
+			name: "normal",
+			pod: &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: corev1.NamespaceDefault,
+					Name:      "test-pod-1",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			suit := newPluginTestSuit(t, nil)
+			p, err := suit.proxyNew(suit.args, suit.Handle)
+			assert.NotNil(t, p)
+			assert.Nil(t, err)
+
+			plg := p.(*Plugin)
+			suit.start()
+			_, err = suit.Handle.ClientSet().CoreV1().Pods(tt.pod.Namespace).Create(context.TODO(), tt.pod, metav1.CreateOptions{})
+			assert.NoError(t, err)
+
+			status := plg.PreBind(context.TODO(), framework.NewCycleState(), tt.pod, tt.nodeName)
+			assert.Nil(t, status)
+
+			newPod, err := suit.Handle.ClientSet().CoreV1().Pods(tt.pod.Namespace).Get(context.TODO(), tt.pod.Name, metav1.GetOptions{})
+			assert.NoError(t, err)
+			assert.Equal(t, tt.nodeName, newPod.Labels[extunified.K8sLabelScheduleNodeName])
+			assert.NotNil(t, newPod.Annotations[extunified.AnnotationSchedulerUpdateTime])
+			assert.NotNil(t, newPod.Annotations[extunified.AnnotationSchedulerBindTime])
+			assert.Equal(t, newPod.Annotations[extunified.AnnotationSchedulerUpdateTime], newPod.Annotations[extunified.AnnotationSchedulerBindTime])
+		})
+	}
+}
