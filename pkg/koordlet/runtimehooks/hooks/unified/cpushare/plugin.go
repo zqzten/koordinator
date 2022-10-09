949fdb890944ab0e16f301119fe2fdd71ae23df1
wangjianyu <zmsjianyu@gmail.com>
2022-10-09 21:01:35 +0800

    koordlet: set container level CPUShare for ignored container resource request
    Signed-off-by: wangjianyu <zmsjianyu@gmail.com>

---
+++ pkg/koordlet/runtimehooks/hooks/unified/cpushare/plugin.go
@@ -0,0 +1,70 @@
+/*
+Copyright 2022 The Koordinator Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cpushare
+
+import (
+	"fmt"
+
+	"k8s.io/klog/v2"
+
+	extunified "github.com/koordinator-sh/koordinator/apis/extension/unified"
+	"github.com/koordinator-sh/koordinator/pkg/koordlet/runtimehooks/protocol"
+	"github.com/koordinator-sh/koordinator/pkg/koordlet/runtimehooks/reconciler"
+	sysutil "github.com/koordinator-sh/koordinator/pkg/util/system"
+)
+
+const (
+	Name        = "UnifiedCPUShare"
+	description = "set container level CPUShares for ignored container resource request"
+)
+
+type plugin struct {
+}
+
+func (p *plugin) Register() {
+	klog.V(5).Infof("register hook %v", Name)
+	reconciler.RegisterCgroupReconciler(reconciler.ContainerLevel, sysutil.CPUShares, SetContainerShares,
+		description)
+}
+
+var singleton *plugin
+
+func Object() *plugin {
+	if singleton == nil {
+		singleton = &plugin{}
+	}
+	return singleton
+}
+
+func SetContainerShares(proto protocol.HooksProtocol) error {
+	containerCtx := proto.(*protocol.ContainerContext)
+	if containerCtx == nil {
+		return fmt.Errorf("container protocol is nil for plugin %v", Name)
+	}
+	containerReq := containerCtx.Request
+	containerCgroup, err := extunified.GetCustomCgroupByContainerName(containerReq.PodAnnotations, containerReq.ContainerMeta.Name)
+	if err != nil {
+		return err
+	}
+	if containerCgroup == nil || containerCgroup.CPUShares == nil {
+		klog.V(5).Infof("container %v/%v/%v doesn't have containerCgroup or containerCgroup.CPUShares is nil ", containerReq.PodMeta.Namespace,
+			containerReq.PodMeta.Name, containerReq.ContainerMeta.Name)
+		return nil
+	}
+	containerCtx.Response.Resources.CPUShares = containerCgroup.CPUShares
+	return nil
+}
---
+++ pkg/koordlet/runtimehooks/unified_config.go
@@ -0,0 +1,45 @@
+/*
+Copyright 2022 The Koordinator Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package runtimehooks
+
+import (
+	"k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/component-base/featuregate"
+
+	unifiedcpushare "github.com/koordinator-sh/koordinator/pkg/koordlet/runtimehooks/hooks/unified/cpushare"
+)
+
+const (
+	UnifiedCPUShare featuregate.Feature = unifiedcpushare.Name
+)
+
+var (
+	unifiedRuntimeHooksFG = map[featuregate.Feature]featuregate.FeatureSpec{
+		UnifiedCPUShare: {Default: false, PreRelease: featuregate.Alpha},
+	}
+
+	unifiedRuntimeHookPlugins = map[featuregate.Feature]HookPlugin{
+		UnifiedCPUShare: unifiedcpushare.Object(),
+	}
+)
+
+func init() {
+	runtime.Must(DefaultMutableRuntimeHooksFG.Add(unifiedRuntimeHooksFG))
+	for k, v := range unifiedRuntimeHookPlugins {
+		runtimeHookPlugins[k] = v
+	}
+}
