144cbc07e286a3afc34ac461ac339c4d7e26debd
wangjianyu <zmsjianyu@gmail.com>
2022-10-09 21:01:35 +0800

    koordlet: set container level CPUShare for ignored container resource request
    Signed-off-by: wangjianyu <zmsjianyu@gmail.com>

---
+++ apis/extension/unified/container.go
@@ -0,0 +1,74 @@
+/*
+Copyright 2022 The Koordinator Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package unified
+
+import (
+	"encoding/json"
+
+	corev1 "k8s.io/api/core/v1"
+
+	"github.com/koordinator-sh/koordinator/apis/extension"
+)
+
+const (
+	AnnotationContainerResourceRequest = extension.DomainPrefix + "containerResourceRequest"
+	EnvSigmaIgnoreResource             = "SIGMA_IGNORE_RESOURCE"
+)
+
+type ContainerResourceRequest struct {
+	Name            string              `json:"name,omitempty"`
+	ResourceRequest corev1.ResourceList `json:"resourceRequest,omitempty"`
+}
+
+func GetResourceRequestByContainerName(annotations map[string]string, containerName string) (*ContainerResourceRequest, error) {
+	requests, err := GetResourceRequests(annotations)
+	if err != nil {
+		return nil, err
+	}
+	if requests == nil {
+		return nil, nil
+	}
+	return requests[containerName], nil
+}
+
+func GetResourceRequests(annotations map[string]string) (map[string]*ContainerResourceRequest, error) {
+	if annotations == nil {
+		return nil, nil
+	}
+	rawRequests, ok := annotations[AnnotationContainerResourceRequest]
+	if !ok || len(rawRequests) == 0 {
+		return nil, nil
+	}
+	var requests []*ContainerResourceRequest
+	if err := json.Unmarshal([]byte(rawRequests), &requests); err != nil {
+		return nil, err
+	}
+	result := map[string]*ContainerResourceRequest{}
+	for _, request := range requests {
+		result[request.Name] = request
+	}
+	return result, nil
+}
+
+func IsContainerIgnoreResourceByEnvs(containerEnvs map[string]string) bool {
+	for name, value := range containerEnvs {
+		if name == EnvSigmaIgnoreResource && value == "true" {
+			return true
+		}
+	}
+	return false
+}
