ef93e2194f6eb8585ca3cb3adb2bfcf1376d19e5
Joseph <joseph.t.lee@outlook.com>
2022-10-27 00:26:24 +0800

    chore: update ASI docker file with logrotate
    Signed-off-by: Joseph <joseph.t.lee@outlook.com>

---
+++ docker-asi/start-koord-manager.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+mkdir -p /log/
+chmod 755 /log/
+
+args="$@"
+if [ $# -le 1 ]; then
+    args="--logtostderr --v=4"
+fi
+
+set -a
+: ${ALI_LOGROTATE_COMPRESS:=true} # compress logs
+: ${ALI_LOGROTATE_MAX_BACKUPS:=8000} # save 8000 log files, 8000 * 28MB = 218.75GB
+: ${ALI_LOGROTATE_MAX_AGE:=7} # save 7 days
+set +a
+
+pidof koord-manager || {
+/koord-manager $args 2>&1 | /logrotate --file /log/koord-manager.log
+}
