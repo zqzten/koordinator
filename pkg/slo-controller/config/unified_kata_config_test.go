077f9c006ede32c752a739edcb5eb646b8032856
Heyan Liu <liuhoyen.lhy@alibaba-inc.com>
2023-02-10 16:27:26 +0800

    koord-manager: add config for kata resource calculator
    Signed-off-by: Heyan Liu <liuhoyen.lhy@alibaba-inc.com>

---
+++ pkg/slo-controller/config/unified_kata_config_test.go
@@ -0,0 +1,168 @@
+/*
+Copyright 2022 The Koordinator Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package config
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/tools/record"
+	"k8s.io/utils/pointer"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	"github.com/koordinator-sh/koordinator/apis/extension"
+)
+
+func Test_syncColocationConfigWithKataResourceIfChanged(t *testing.T) {
+	RegisterDefaultColocationExtension(KataResExtKey, defaultKataResourceConfig)
+	oldCfg := *NewDefaultColocationCfg()
+	oldCfg.MemoryReclaimThresholdPercent = pointer.Int64Ptr(40)
+
+	type fields struct {
+		config *colocationCfgCache
+	}
+	type args struct {
+		configMap *corev1.ConfigMap
+	}
+	type wants struct {
+		available   bool
+		errorStatus bool
+		kataCfg     *KataResourceConfig
+	}
+	tests := []struct {
+		name        string
+		fields      fields
+		args        args
+		wantChanged bool
+		wantField   *wants
+	}{
+		{
+			name:   "no colocation config in configMap, no old cfg in cache, cfg will be changed to default cfg",
+			fields: fields{config: &colocationCfgCache{}},
+			args: args{configMap: &corev1.ConfigMap{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "ConfigMap",
+					APIVersion: "v1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      SLOCtrlConfigMap,
+					Namespace: ConfigNameSpace,
+				},
+				Data: map[string]string{},
+			}},
+			wantChanged: true,
+			wantField: &wants{
+				available:   true,
+				errorStatus: false,
+				kataCfg:     defaultKataResourceConfig,
+			},
+		},
+		{
+			name:   "no colocation config in configMap, old cfg is in cache, cfg will be changed to default cfg",
+			fields: fields{config: &colocationCfgCache{colocationCfg: oldCfg}},
+			args: args{configMap: &corev1.ConfigMap{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "ConfigMap",
+					APIVersion: "v1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      SLOCtrlConfigMap,
+					Namespace: ConfigNameSpace,
+				},
+				Data: map[string]string{},
+			}},
+			wantChanged: true,
+			wantField: &wants{
+				available:   true,
+				errorStatus: false,
+				kataCfg:     defaultKataResourceConfig,
+			},
+		},
+		{
+			name:   "no colocation config in configmap, cache has been set to default cfg",
+			fields: fields{config: &colocationCfgCache{colocationCfg: *NewDefaultColocationCfg()}},
+			args: args{configMap: &corev1.ConfigMap{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "ConfigMap",
+					APIVersion: "v1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      SLOCtrlConfigMap,
+					Namespace: ConfigNameSpace,
+				},
+				Data: map[string]string{},
+			}},
+			wantChanged: false,
+			wantField: &wants{
+				available:   true,
+				errorStatus: false,
+				kataCfg:     defaultKataResourceConfig,
+			},
+		},
+		{
+			name: "validate and merge partial config with the default config",
+			fields: fields{config: &colocationCfgCache{
+				colocationCfg: oldCfg,
+				available:     true,
+			}},
+			args: args{configMap: &corev1.ConfigMap{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "ConfigMap",
+					APIVersion: "v1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      SLOCtrlConfigMap,
+					Namespace: ConfigNameSpace,
+				},
+				Data: map[string]string{
+					extension.ColocationConfigKey: "{\"metricAggregateDurationSeconds\":60,\"cpuReclaimThresholdPercent\":70," +
+						"\"memoryReclaimThresholdPercent\":70,\"updateTimeThresholdSeconds\":100," +
+						"\"metricReportIntervalSeconds\":20," +
+						"\"extensions\":{\"kataResource\":{\"enable\":true}}}",
+				},
+			}},
+			wantChanged: true,
+			wantField: &wants{
+				kataCfg:     &KataResourceConfig{Enable: pointer.BoolPtr(true)},
+				available:   true,
+				errorStatus: false,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fakeClient := fake.NewClientBuilder().WithScheme(scheme.Scheme).Build()
+			p := NewColocationHandlerForConfigMapEvent(fakeClient, *NewDefaultColocationCfg(), &record.FakeRecorder{})
+			p.cfgCache = colocationCfgCache{
+				colocationCfg: tt.fields.config.colocationCfg,
+				available:     tt.fields.config.available,
+			}
+			got := p.syncColocationCfgIfChanged(tt.args.configMap)
+			assert.Equal(t, tt.wantChanged, got)
+			assert.Equal(t, tt.wantField.available, p.IsCfgAvailable())
+			assert.Equal(t, tt.wantField.errorStatus, p.IsErrorStatus())
+			gotStrategy := p.GetCfgCopy().ColocationStrategy
+			gotKataExtCfg, err := ParseKataResourceConfig(&gotStrategy)
+			assert.NoError(t, err, "parse kata resource config failed")
+			assert.Equal(t, tt.wantField.kataCfg, gotKataExtCfg)
+		})
+	}
+	UnregisterDefaultColocationExtension(KataResExtKey)
+}
