e252a2d8c2e25129cde8c90d4b287ab4399deb3c
Joseph <joseph.t.lee@outlook.com>
2023-03-08 23:52:32 +0800

    transform unified non-prod pod resource spec
    Signed-off-by: Joseph <joseph.t.lee@outlook.com>

--- pkg/util/transformer/transformer_test.go
+++ pkg/util/transformer/transformer_test.go
@@ -6,10 +6,128 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/pointer"
 	syncerconsts "sigs.k8s.io/cluster-api-provider-nested/virtualcluster/pkg/syncer/constants"
+
+	"github.com/koordinator-sh/koordinator/apis/extension"
+	"github.com/koordinator-sh/koordinator/apis/extension/unified"
 )
+
+func TestTransformSigmaIgnoreResourceContainers(t *testing.T) {
+	resources := corev1.ResourceList{
+		corev1.ResourceCPU:    resource.MustParse("4000m"),
+		corev1.ResourceMemory: resource.MustParse("4Gi"),
+	}
+	updatedResources := corev1.ResourceList{
+		corev1.ResourceCPU:    *resource.NewQuantity(0, resource.DecimalSI),
+		corev1.ResourceMemory: *resource.NewQuantity(0, resource.BinarySI),
+	}
+
+	tests := []struct {
+		name        string
+		pod         *corev1.Pod
+		expectedPod *corev1.Pod
+	}{
+		{
+			name: "normal pods",
+			pod: &corev1.Pod{
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Resources: corev1.ResourceRequirements{
+								Limits:   resources.DeepCopy(),
+								Requests: resources.DeepCopy(),
+							},
+						},
+						{
+							Resources: corev1.ResourceRequirements{
+								Limits:   resources.DeepCopy(),
+								Requests: resources.DeepCopy(),
+							},
+						},
+					},
+				},
+			},
+			expectedPod: &corev1.Pod{
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Resources: corev1.ResourceRequirements{
+								Limits:   resources.DeepCopy(),
+								Requests: resources.DeepCopy(),
+							},
+						},
+						{
+							Resources: corev1.ResourceRequirements{
+								Limits:   resources.DeepCopy(),
+								Requests: resources.DeepCopy(),
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "pod with SIGMA_IGNORE_RESOURCE",
+			pod: &corev1.Pod{
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Env: []corev1.EnvVar{
+								{
+									Name:  unified.EnvSigmaIgnoreResource,
+									Value: "true",
+								},
+							},
+							Resources: corev1.ResourceRequirements{
+								Limits:   resources.DeepCopy(),
+								Requests: resources.DeepCopy(),
+							},
+						},
+						{
+							Resources: corev1.ResourceRequirements{
+								Limits:   resources.DeepCopy(),
+								Requests: resources.DeepCopy(),
+							},
+						},
+					},
+				},
+			},
+			expectedPod: &corev1.Pod{
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Env: []corev1.EnvVar{
+								{
+									Name:  unified.EnvSigmaIgnoreResource,
+									Value: "true",
+								},
+							},
+							Resources: corev1.ResourceRequirements{
+								Limits:   resources.DeepCopy(),
+								Requests: updatedResources.DeepCopy(),
+							},
+						},
+						{
+							Resources: corev1.ResourceRequirements{
+								Limits:   resources.DeepCopy(),
+								Requests: resources.DeepCopy(),
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			TransformSigmaIgnoreResourceContainers(tt.pod)
+			assert.Equal(t, tt.expectedPod, tt.pod)
+		})
+	}
+}
 
 func TestTransformTenantPod(t *testing.T) {
 	tests := []struct {
@@ -43,6 +161,88 @@ func TestTransformTenantPod(t *testing.T) {
 			}
 			TransformTenantPod(pod)
 			assert.Equal(t, tt.ownerReference, pod.OwnerReferences)
+		})
+	}
+}
+
+func TestTransformNonProdPodResourceSpec(t *testing.T) {
+	resources := corev1.ResourceList{
+		corev1.ResourceCPU:    resource.MustParse("4000m"),
+		corev1.ResourceMemory: resource.MustParse("4Gi"),
+	}
+	batchResources := corev1.ResourceList{
+		extension.BatchCPU:    *resource.NewQuantity(4000, resource.DecimalSI),
+		extension.BatchMemory: resource.MustParse("4Gi"),
+	}
+
+	tests := []struct {
+		name        string
+		pod         *corev1.Pod
+		expectedPod *corev1.Pod
+	}{
+		{
+			name: "prod pod",
+			pod: &corev1.Pod{
+				Spec: corev1.PodSpec{
+					Priority: pointer.Int32(extension.PriorityProdValueMax),
+					Containers: []corev1.Container{
+						{
+							Resources: corev1.ResourceRequirements{
+								Limits:   resources.DeepCopy(),
+								Requests: resources.DeepCopy(),
+							},
+						},
+					},
+				},
+			},
+			expectedPod: &corev1.Pod{
+				Spec: corev1.PodSpec{
+					Priority: pointer.Int32(extension.PriorityProdValueMax),
+					Containers: []corev1.Container{
+						{
+							Resources: corev1.ResourceRequirements{
+								Limits:   resources.DeepCopy(),
+								Requests: resources.DeepCopy(),
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "batch pod",
+			pod: &corev1.Pod{
+				Spec: corev1.PodSpec{
+					Priority: pointer.Int32(extension.PriorityBatchValueMin),
+					Containers: []corev1.Container{
+						{
+							Resources: corev1.ResourceRequirements{
+								Limits:   resources.DeepCopy(),
+								Requests: resources.DeepCopy(),
+							},
+						},
+					},
+				},
+			},
+			expectedPod: &corev1.Pod{
+				Spec: corev1.PodSpec{
+					Priority: pointer.Int32(extension.PriorityBatchValueMin),
+					Containers: []corev1.Container{
+						{
+							Resources: corev1.ResourceRequirements{
+								Limits:   batchResources.DeepCopy(),
+								Requests: batchResources.DeepCopy(),
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			TransformNonProdPodResourceSpec(tt.pod)
+			assert.Equal(t, tt.expectedPod, tt.pod)
 		})
 	}
 }
