8aebd3f6743978434d74ff61c95c8b610c5caa67
Joseph <joseph.t.lee@outlook.com>
2023-03-13 17:03:53 +0800

    bump to upstream, remove custom GetQuotaName
    Signed-off-by: Joseph <joseph.t.lee@outlook.com>

--- pkg/scheduler/plugins/elasticquota/unified/unified.go
+++ pkg/scheduler/plugins/elasticquota/unified/unified.go
@@ -17,23 +17,13 @@ limitations under the License.
 package unified
 
 import (
-	"strings"
-
 	cosextension "gitlab.alibaba-inc.com/cos/unified-resource-api/apis/extension"
-	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/labels"
-	"k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/sets"
-	"sigs.k8s.io/scheduler-plugins/pkg/generated/listers/scheduling/v1alpha1"
 
-	"github.com/koordinator-sh/koordinator/apis/extension"
-	"github.com/koordinator-sh/koordinator/apis/extension/ack"
-	"github.com/koordinator-sh/koordinator/pkg/scheduler/plugins/elasticquota"
 	"github.com/koordinator-sh/koordinator/pkg/scheduler/plugins/elasticquota/gpumodel"
 )
 
 func init() {
-	elasticquota.GetQuotaName = GetQuotaName
 	gpumodel.GPUResourceMemRatio = cosextension.GPUResourceMemRatio
 	gpumodel.GPUResourceMem = cosextension.GPUResourceMem
 	gpumodel.GPUResourceCardRatio = cosextension.GPUResourceCardRatio
@@ -73,27 +63,3 @@ var PercentageGPUNamesForPod = sets.NewString(
 	cosextension.GPUResourceMemRatio,
 	cosextension.GPUResourceCore,
 )
-
-func GetQuotaName(quotaLister v1alpha1.ElasticQuotaLister, pod *v1.Pod) string {
-	eqList, err := quotaLister.List(labels.Everything())
-	if err != nil {
-		return extension.DefaultQuotaName
-	}
-	for _, eq := range eqList {
-		namespaces := eq.Annotations[ack.AnnotationQuotaNamespaces]
-		if strings.Contains(namespaces, pod.Namespace) {
-			return eq.Name
-		}
-	}
-
-	list, err := quotaLister.ElasticQuotas(pod.Namespace).List(labels.Everything())
-	if err != nil {
-		runtime.HandleError(err)
-		return extension.DefaultQuotaName
-	}
-	if len(list) == 0 {
-		return extension.DefaultQuotaName
-	}
-	// todo when elastic quota supports multiple instances in a namespace, modify this
-	return list[0].Name
-}
