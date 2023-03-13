ccee57d49bc9990eabba40f1c3e6c6d1613bf040
Joseph <joseph.t.lee@outlook.com>
2023-03-13 17:03:53 +0800

    bump to upstream, remove custom GetQuotaName
    Signed-off-by: Joseph <joseph.t.lee@outlook.com>

--- pkg/scheduler/plugins/unified/elasticquotatree/elastic_quota_tree_adaptor.go
+++ pkg/scheduler/plugins/unified/elasticquotatree/elastic_quota_tree_adaptor.go
@@ -44,7 +44,6 @@ import (
 	"sigs.k8s.io/scheduler-plugins/pkg/generated/listers/scheduling/v1alpha1"
 
 	"github.com/koordinator-sh/koordinator/apis/extension"
-	"github.com/koordinator-sh/koordinator/apis/extension/ack"
 	"github.com/koordinator-sh/koordinator/pkg/scheduler/frameworkext"
 	"github.com/koordinator-sh/koordinator/pkg/util"
 )
@@ -169,7 +168,7 @@ func (p *Plugin) OnQuotaTreeAdd(obj interface{}) {
 	if quotaTree == nil {
 		return
 	}
-	p.createOrUpdateQuotaByTraversalTree("root", quotaTree.Spec.Root)
+	p.createOrUpdateQuotaByTraversalTree(extension.RootQuotaName, quotaTree.Spec.Root)
 	p.deleteElasticQuota(quotaTree.Spec.Root)
 	klog.Infof("QuotaTree %v", quotaTree)
 }
@@ -371,5 +370,4 @@ func parseElasticQuotaSpec(quota *apiv1alpha1.ElasticQuota, spec v1beta1.Elastic
 		quota.Labels[extension.LabelQuotaIsParent] = "true"
 	}
 	quota.Labels[extension.LabelQuotaParent] = parent
-	quota.Annotations[ack.AnnotationQuotaNamespaces] = strings.Join(spec.Namespaces, ",")
 }
