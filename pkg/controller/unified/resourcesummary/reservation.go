91988979398728247cd838dd718e6b68c8e2c732
wangjianyu.wjy <wangjianyu.wjy@alibaba-inc.com>
2023-04-03 18:51:37 +0800

    koord-manager: resourcesummary reservation
    Signed-off-by: wangjianyu.wjy <wangjianyu.wjy@alibaba-inc.com>

---
+++ pkg/controller/unified/resourcesummary/reservation.go
@@ -0,0 +1,52 @@
+package resourcesummary
+
+import (
+	"context"
+
+	"gitlab.alibaba-inc.com/unischeduler/api/apis/scheduling/v1beta1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	quotav1 "k8s.io/apiserver/pkg/quota/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	extunified "github.com/koordinator-sh/koordinator/apis/extension/unified"
+	schedulingv1alpha1 "github.com/koordinator-sh/koordinator/apis/scheduling/v1alpha1"
+	utilclient "github.com/koordinator-sh/koordinator/pkg/util/client"
+	reservationutil "github.com/koordinator-sh/koordinator/pkg/util/reservation"
+)
+
+func (r *Reconciler) getReservationForCandidateNodes(ctx context.Context, candidateNodes *corev1.NodeList,
+) (map[string][]*schedulingv1alpha1.Reservation, error) {
+	nodeOwnedReservations := map[string][]*schedulingv1alpha1.Reservation{}
+	for _, node := range candidateNodes.Items {
+		reservationList := &schedulingv1alpha1.ReservationList{}
+		if err := r.Client.List(ctx, reservationList, &client.ListOptions{
+			FieldSelector: fields.OneTermEqualSelector("status.nodeName", node.Name),
+		}, utilclient.DisableDeepCopy); err != nil {
+			return nil, err
+		}
+		for i := range reservationList.Items {
+			if reservationList.Items[i].Status.Phase == schedulingv1alpha1.ReservationAvailable {
+				nodeOwnedReservations[node.Name] = append(nodeOwnedReservations[node.Name], &reservationList.Items[i])
+			}
+		}
+	}
+	return nodeOwnedReservations, nil
+}
+
+func GetReservationPriorityResource(reservation *schedulingv1alpha1.Reservation, node *corev1.Node, gpuCapacity corev1.ResourceList, resourceNames ...corev1.ResourceName) (used, capacity, free v1beta1.PodPriorityUsed) {
+	pod := reservationutil.NewReservePod(reservation)
+	unifiedPriority := extunified.GetUnifiedPriorityClass(pod)
+	allocatable := priorityPodRequestedToNormal(reservation.Status.Allocatable, unifiedPriority)
+	allocated := priorityPodRequestedToNormal(reservation.Status.Allocated, unifiedPriority)
+
+	scaleCPUAndACU(pod, node, allocatable)
+	scaleCPUAndACU(pod, node, allocated)
+	fillGPUResource(allocatable, gpuCapacity)
+	fillGPUResource(allocated, gpuCapacity)
+	if len(resourceNames) > 0 {
+		allocatable = quotav1.Mask(allocatable, resourceNames)
+		allocated = quotav1.Mask(allocated, resourceNames)
+	}
+	return v1beta1.PodPriorityUsed{PriorityClass: unifiedPriority, Allocated: allocated}, v1beta1.PodPriorityUsed{PriorityClass: unifiedPriority, Allocated: allocatable}, v1beta1.PodPriorityUsed{PriorityClass: unifiedPriority, Allocated: quotav1.Subtract(allocatable, allocated)}
+}
--- pkg/util/fieldindex/register.go
+++ pkg/util/fieldindex/register.go
@@ -26,6 +26,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	apiv1alpha1 "sigs.k8s.io/scheduler-plugins/pkg/apis/scheduling/v1alpha1"
 
+	schedulingv1alpha1 "github.com/koordinator-sh/koordinator/apis/scheduling/v1alpha1"
 	"github.com/koordinator-sh/koordinator/apis/extension"
 )
 
@@ -83,6 +84,21 @@ var indexDescriptors = []fieldIndexDescriptor{
 				return []string{}
 			}
 			return extension.GetAnnotationQuotaNamespaces(eq)
+		},
+	},
+	{
+		description: "index reservation by status.nodeName",
+		obj:         &schedulingv1alpha1.Reservation{},
+		field:       "status.nodeName",
+		indexerFunc: func(obj client.Object) []string {
+			reservation, ok := obj.(*schedulingv1alpha1.Reservation)
+			if !ok {
+				return []string{}
+			}
+			if len(reservation.Status.NodeName) == 0 {
+				return []string{}
+			}
+			return []string{reservation.Status.NodeName}
 		},
 	},
 }
