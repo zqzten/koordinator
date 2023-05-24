b277493eebb6b69b5f8cb730927a95ea18d33b89
wangjianyu.wjy <wangjianyu.wjy@alibaba-inc.com>
2023-05-24 11:43:40 +0800

    koord-scheduler: make honor taintToleration switchable in topology scheduling
    Signed-off-by: wangjianyu.wjy <wangjianyu.wjy@alibaba-inc.com>

---
+++ pkg/scheduler/plugins/unified/podtopologyspread/common_test.go
@@ -0,0 +1,174 @@
+package podtopologyspread
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	uniext "gitlab.alibaba-inc.com/unischeduler/api/apis/extension"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	utilfeature "k8s.io/apiserver/pkg/util/feature"
+
+	extunified "github.com/koordinator-sh/koordinator/apis/extension/unified"
+	"github.com/koordinator-sh/koordinator/pkg/features"
+	"github.com/koordinator-sh/koordinator/pkg/util/feature"
+)
+
+func Test_tolerationTolerateNodeFn(t *testing.T) {
+	tests := []struct {
+		name                        string
+		defaultHonorTaintToleration bool
+		pod                         *corev1.Pod
+		node                        *corev1.Node
+		want                        bool
+	}{
+		{
+			name:                        "DefaultHonorTaintTolerationInTopologySpread disabled, pass",
+			defaultHonorTaintToleration: false,
+			pod: &corev1.Pod{
+				TypeMeta:   metav1.TypeMeta{},
+				ObjectMeta: metav1.ObjectMeta{},
+				Spec:       corev1.PodSpec{},
+				Status:     corev1.PodStatus{},
+			},
+			node: &corev1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test-node",
+				},
+				Spec: corev1.NodeSpec{
+					Taints: []corev1.Taint{
+						{
+							Key:    "test-taint",
+							Effect: corev1.TaintEffectNoSchedule,
+						},
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name:                        "DefaultHonorTaintTolerationInTopologySpread enabled, normal node, tolerated",
+			defaultHonorTaintToleration: true,
+			pod: &corev1.Pod{
+				Spec: corev1.PodSpec{
+					Tolerations: []corev1.Toleration{
+						{
+							Key:      "test-taint",
+							Operator: corev1.TolerationOpExists,
+							Effect:   corev1.TaintEffectNoSchedule,
+						},
+					},
+				},
+			},
+			node: &corev1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test-node",
+				},
+				Spec: corev1.NodeSpec{
+					Taints: []corev1.Taint{
+						{
+							Key:    "test-taint",
+							Effect: corev1.TaintEffectNoSchedule,
+						},
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name:                        "DefaultHonorTaintTolerationInTopologySpread enabled, normal node, unTolerated",
+			defaultHonorTaintToleration: true,
+			pod: &corev1.Pod{
+				Spec: corev1.PodSpec{},
+			},
+			node: &corev1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test-node",
+				},
+				Spec: corev1.NodeSpec{
+					Taints: []corev1.Taint{
+						{
+							Key:    "test-taint",
+							Effect: corev1.TaintEffectNoSchedule,
+						},
+					},
+				},
+			},
+			want: false,
+		},
+		{
+			name:                        "DefaultHonorTaintTolerationInTopologySpread enabled, vknode, tolerated",
+			defaultHonorTaintToleration: true,
+			pod: &corev1.Pod{
+				TypeMeta: metav1.TypeMeta{},
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						uniext.LabelECIAffinity: uniext.ECIRequired,
+					},
+				},
+				Spec:   corev1.PodSpec{},
+				Status: corev1.PodStatus{},
+			},
+			node: &corev1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test-node",
+					Labels: map[string]string{
+						uniext.LabelNodeType: uniext.VKType,
+					},
+				},
+				Spec: corev1.NodeSpec{
+					Taints: []corev1.Taint{
+						{
+							Key:    extunified.VKTaintKey,
+							Effect: corev1.TaintEffectNoSchedule,
+						},
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name:                        "DefaultHonorTaintTolerationInTopologySpread enabled, vknode, unTolerated",
+			defaultHonorTaintToleration: true,
+			pod: &corev1.Pod{
+				TypeMeta: metav1.TypeMeta{},
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						uniext.LabelECIAffinity: uniext.ECIRequired,
+					},
+				},
+				Spec: corev1.PodSpec{
+					Tolerations: []corev1.Toleration{},
+				},
+				Status: corev1.PodStatus{},
+			},
+			node: &corev1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test-node",
+					Labels: map[string]string{
+						uniext.LabelNodeType: uniext.VKType,
+					},
+				},
+				Spec: corev1.NodeSpec{
+					Taints: []corev1.Taint{
+						{
+							Key:    extunified.VKTaintKey,
+							Effect: corev1.TaintEffectNoSchedule,
+						},
+						{
+							Key:    "test-taint",
+							Effect: corev1.TaintEffectNoSchedule,
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			defer feature.SetFeatureGateDuringTest(t, utilfeature.DefaultMutableFeatureGate, features.DefaultHonorTaintTolerationInTopologySpread, tt.defaultHonorTaintToleration)()
+			tolerationTolerateNode := tolerationTolerateNodeFn(tt.pod)
+			assert.Equal(t, tt.want, tolerationTolerateNode(tt.node))
+		})
+	}
+}
