0d554701f0d9447ba7c1fd3c931dcc9aadd55c6a
wangjianyu.wjy <wangjianyu.wjy@alibaba-inc.com>
2023-07-06 17:25:49 +0800

    koord-scheduler: support inplace update
    Signed-off-by: wangjianyu.wjy <wangjianyu.wjy@alibaba-inc.com>

---
+++ pkg/scheduler/plugins/unified/inplaceupdate/pod_eventhandler_test.go
@@ -0,0 +1,117 @@
+package inplaceupdate
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	uniext "gitlab.alibaba-inc.com/unischeduler/api/apis/extension"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func Test_podHasUnhandledUpdateReq(t *testing.T) {
+	tests := []struct {
+		name                string
+		resourceUpdateSpec  *uniext.ResourceUpdateSpec
+		resourceUpdateState *uniext.ResourceUpdateSchedulerState
+		want                bool
+	}{
+		{
+			name: "pod specify update spec, but scheduler state not patched",
+			resourceUpdateSpec: &uniext.ResourceUpdateSpec{
+				Version: "2",
+			},
+			want: true,
+		},
+		{
+			name: "pod specify update spec, but scheduler state not updated",
+			resourceUpdateSpec: &uniext.ResourceUpdateSpec{
+				Version: "2",
+			},
+			resourceUpdateState: &uniext.ResourceUpdateSchedulerState{
+				Version: "1",
+			},
+			want: true,
+		},
+		{
+			name: "pod specify update spec, but scheduler state not updated",
+			resourceUpdateSpec: &uniext.ResourceUpdateSpec{
+				Version: "2",
+			},
+			resourceUpdateState: &uniext.ResourceUpdateSchedulerState{
+				Version: "2",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			pod := &corev1.Pod{ObjectMeta: metav1.ObjectMeta{Annotations: map[string]string{}}}
+			if tt.resourceUpdateSpec != nil {
+				assert.NoError(t, uniext.SetResourceUpdateSpec(pod.Annotations, tt.resourceUpdateSpec))
+			}
+			if tt.resourceUpdateState != nil {
+				assert.NoError(t, uniext.SetResourceUpdateSchedulerState(pod.Annotations, tt.resourceUpdateState))
+			}
+			if got := podHasUnhandledUpdateReq(pod); got != tt.want {
+				t.Errorf("podHasUnhandledUpdateReq() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_podHasUpdateReq(t *testing.T) {
+	tests := []struct {
+		name                  string
+		oldResourceUpdateSpec *uniext.ResourceUpdateSpec
+		newResourceUpdateSpec *uniext.ResourceUpdateSpec
+		want                  bool
+	}{
+		{
+			name: "pod specify update spec at first",
+			oldResourceUpdateSpec: &uniext.ResourceUpdateSpec{
+				Version: "1",
+			},
+			want: true,
+		},
+		{
+			name: "pod update updateSpec",
+			oldResourceUpdateSpec: &uniext.ResourceUpdateSpec{
+				Version: "1",
+			},
+			newResourceUpdateSpec: &uniext.ResourceUpdateSpec{
+				Version: "2",
+			},
+			want: true,
+		},
+		{
+			name: "updateSpec unchanged",
+			oldResourceUpdateSpec: &uniext.ResourceUpdateSpec{
+				Version: "1",
+			},
+			newResourceUpdateSpec: &uniext.ResourceUpdateSpec{
+				Version: "1",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			oldPod := &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{Annotations: map[string]string{}},
+			}
+			if tt.oldResourceUpdateSpec != nil {
+				assert.NoError(t, uniext.SetResourceUpdateSpec(oldPod.Annotations, tt.oldResourceUpdateSpec))
+			}
+			newPod := &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{Annotations: map[string]string{}},
+			}
+			if tt.newResourceUpdateSpec != nil {
+				assert.NoError(t, uniext.SetResourceUpdateSpec(newPod.Annotations, tt.newResourceUpdateSpec))
+			}
+			if got := podHasUpdateReq(oldPod, newPod); got != tt.want {
+				t.Errorf("podHasUpdateReq() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
