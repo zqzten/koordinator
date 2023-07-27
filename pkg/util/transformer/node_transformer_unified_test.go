5eb92bb33193a389585dc63b97e307c9285682f3
wangjianyu.wjy <wangjianyu.wjy@alibaba-inc.com>
2023-07-27 10:17:08 +0800

    transformer: fix transformer overquota
    Signed-off-by: wangjianyu.wjy <wangjianyu.wjy@alibaba-inc.com>

--- pkg/util/transformer/node_transformer_unified_test.go
+++ pkg/util/transformer/node_transformer_unified_test.go
@@ -17,6 +17,7 @@ limitations under the License.
 package transformer
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -24,10 +25,55 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	quotav1 "k8s.io/apiserver/pkg/quota/v1"
 	"k8s.io/kubernetes/pkg/scheduler/framework"
 
 	apiext "github.com/koordinator-sh/koordinator/apis/extension"
+	extunified "github.com/koordinator-sh/koordinator/apis/extension/unified"
 )
+
+func TestTransformNodeAllocatableWithOverQuota(t *testing.T) {
+	tests := []struct {
+		name                       string
+		nodeLabels                 map[string]string
+		nodeAllocatable            corev1.ResourceList
+		wantTransformedAllocatable corev1.ResourceList
+	}{
+		{
+			name: "cpu 1.5, and memory 2",
+			nodeLabels: map[string]string{
+				extunified.LabelCPUOverQuota:    "1.5",
+				extunified.LabelMemoryOverQuota: "2",
+			},
+			nodeAllocatable: corev1.ResourceList{
+				corev1.ResourceCPU:    resource.MustParse("2"),
+				corev1.ResourceMemory: resource.MustParse("2"),
+			},
+			wantTransformedAllocatable: corev1.ResourceList{
+				corev1.ResourceCPU:    resource.MustParse("3"),
+				corev1.ResourceMemory: resource.MustParse("4"),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			node := &corev1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:   "test-node",
+					Labels: tt.nodeLabels,
+				},
+				Status: corev1.NodeStatus{
+					Allocatable: tt.nodeAllocatable,
+				},
+			}
+			jsonData, err := json.Marshal(tt.nodeAllocatable)
+			assert.NoError(t, err)
+			TransformNodeAllocatableWithOverQuota(node)
+			assert.Equal(t, string(jsonData), node.Annotations[extunified.AnnotationOriginalNodeAllocatable])
+			assert.True(t, quotav1.Equals(tt.wantTransformedAllocatable, node.Status.Allocatable))
+		})
+	}
+}
 
 func Test_transformNodeInfoAllocatable(t *testing.T) {
 	tests := []struct {
