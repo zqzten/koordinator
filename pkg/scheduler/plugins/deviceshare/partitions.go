fece176e462eb715aca75293787a6a2789ddb53f
Joseph <joseph.t.lee@outlook.com>
2023-07-29 16:43:27 +0800

    koord-scheduler: fix H800 default partitions
    Signed-off-by: Joseph <joseph.t.lee@outlook.com>

--- pkg/scheduler/plugins/deviceshare/partitions.go
+++ pkg/scheduler/plugins/deviceshare/partitions.go
@@ -20,55 +20,16 @@ import (
 	corev1 "k8s.io/api/core/v1"
 
 	apiext "github.com/koordinator-sh/koordinator/apis/extension"
+	"github.com/koordinator-sh/koordinator/apis/extension/unified"
 )
-
-// 一些高级的 NVIDIA GPU 型号要求按照一定的组合关系才能获得最佳的 NVLINK 联通带宽。而这种组合关系称为 Partition。
-// 分配时也要按照这个 Partition 分配，Partition 之外的组合关系是无效的，调度器会拒绝调度。
-
-type GPUPartition struct {
-	ID           int
-	NumberOfGPUs int
-	ModuleIDs    []int
-}
-
-var H800PartitionTables = map[int][]GPUPartition{
-	8: {
-		{ID: 0, NumberOfGPUs: 8, ModuleIDs: []int{1, 2, 3, 4, 5, 6, 7, 8}},
-	},
-	4: {
-		{ID: 1, NumberOfGPUs: 4, ModuleIDs: []int{1, 2, 3, 4}},
-		{ID: 2, NumberOfGPUs: 4, ModuleIDs: []int{5, 6, 7, 8}},
-	},
-	2: {
-		{ID: 3, NumberOfGPUs: 2, ModuleIDs: []int{1, 3}},
-		{ID: 4, NumberOfGPUs: 2, ModuleIDs: []int{2, 4}},
-		{ID: 5, NumberOfGPUs: 2, ModuleIDs: []int{5, 7}},
-		{ID: 6, NumberOfGPUs: 2, ModuleIDs: []int{6, 8}},
-	},
-	1: {
-		// keep the following order to reduce fragments
-		{ID: 7, NumberOfGPUs: 1, ModuleIDs: []int{1}},
-		{ID: 8, NumberOfGPUs: 1, ModuleIDs: []int{3}},
-		{ID: 9, NumberOfGPUs: 1, ModuleIDs: []int{2}},
-		{ID: 10, NumberOfGPUs: 1, ModuleIDs: []int{4}},
-		{ID: 11, NumberOfGPUs: 1, ModuleIDs: []int{5}},
-		{ID: 12, NumberOfGPUs: 1, ModuleIDs: []int{7}},
-		{ID: 13, NumberOfGPUs: 1, ModuleIDs: []int{6}},
-		{ID: 14, NumberOfGPUs: 1, ModuleIDs: []int{8}},
-	},
-}
-
-var PartitionTables = map[string]map[int][]GPUPartition{
-	"H800": H800PartitionTables,
-}
 
 func mustAllocateGPUByPartition(node *corev1.Node) bool {
 	model := node.Labels[apiext.LabelGPUModel]
-	_, ok := PartitionTables[model]
+	_, ok := unified.PartitionTables[model]
 	return ok
 }
 
-func getGPUPartitionTable(node *corev1.Node) map[int][]GPUPartition {
+func getGPUPartitionTable(node *corev1.Node) map[int][]unified.GPUPartition {
 	model := node.Labels[apiext.LabelGPUModel]
-	return PartitionTables[model]
+	return unified.PartitionTables[model]
 }
