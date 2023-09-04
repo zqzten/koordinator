e21d1a9d05604279680cc90e4ae38e160af56cae
Joseph <joseph.t.lee@outlook.com>
2023-09-04 13:28:56 +0800

    bump to upstram change
    Signed-off-by: Joseph <joseph.t.lee@outlook.com>

--- pkg/scheduler/plugins/elasticquota/plugin_gpu_test.go
+++ pkg/scheduler/plugins/elasticquota/plugin_gpu_test.go
@@ -40,8 +40,8 @@ func TestPlugin_OnQuotaAdd2(t *testing.T) {
 	suit := newPluginTestSuit(t, nil)
 	p, _ := suit.proxyNew(suit.elasticQuotaArgs, suit.Handle)
 	pl := p.(*Plugin)
-	pl.GetGroupQuotaManager().UpdateClusterTotalResource(createResourceList(501952056, 0))
-	gqm := pl.GetGroupQuotaManager()
+	pl.GetGroupQuotaManager("").UpdateClusterTotalResource(createResourceList(501952056, 0))
+	gqm := pl.GetGroupQuotaManager("")
 	quota := CreateQuota2("1", "", 0, 0, 0, 0, 0, 0, false)
 	quota.Spec.Min = corev1.ResourceList{
 		corev1.ResourceCPU:                    *resource.NewMilliQuantity(100*1000, resource.DecimalSI),
@@ -197,7 +197,7 @@ func TestPlugin_OnNodeAdd2(t *testing.T) {
 				transformer.TransformNodeAllocatableToUnifiedCardRatio(node)
 				eQP.OnNodeAdd(node)
 			}
-			gqm := eQP.GetGroupQuotaManager()
+			gqm := eQP.GetGroupQuotaManager("")
 			assert.NotNil(t, gqm)
 			assert.Equal(t, tt.totalRes, gqm.GetClusterTotalResource())
 		})
@@ -272,7 +272,7 @@ func TestPlugin_OnNodeUpdate2(t *testing.T) {
 				transformer.TransformNodeAllocatableToUnifiedCardRatio(node)
 				plugin.OnNodeUpdate(nodes[i], node)
 			}
-			assert.Equal(t, p.(*Plugin).GetGroupQuotaManager().GetClusterTotalResource(), tt.totalRes)
+			assert.Equal(t, p.(*Plugin).GetGroupQuotaManager("").GetClusterTotalResource(), tt.totalRes)
 		})
 	}
 }
@@ -283,7 +283,7 @@ func TestPlugin_OnNodeDelete2(t *testing.T) {
 	assert.NotNil(t, p)
 	assert.Nil(t, err)
 	eQP := p.(*Plugin)
-	gqp := eQP.GetGroupQuotaManager()
+	gqp := eQP.GetGroupQuotaManager("")
 	assert.NotNil(t, gqp)
 	nodes := []*corev1.Node{
 		{
