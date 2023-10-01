7666b2f8dc2ca152179614a10001a9d553046c69
Joseph <joseph.t.lee@outlook.com>
2023-10-01 14:05:31 +0800

    koord-scheduler: UnifiedVolumeBinding supports provision-denied
    Signed-off-by: Joseph <joseph.t.lee@outlook.com>

---
+++ apis/extension/unified/acs.go
@@ -0,0 +1,11 @@
+package unified
+
+import uniext "gitlab.alibaba-inc.com/unischeduler/api/apis/extension"
+
+const (
+	ACSType = "virtual-cluster-node"
+)
+
+func IsACSVirtualNode(labels map[string]string) bool {
+	return labels[uniext.LabelNodeType] == ACSType
+}
