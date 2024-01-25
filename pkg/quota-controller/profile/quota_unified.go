d19172af0a6bdc2f436f7b87f2e6542c26d8318b
chuanyun.lcy <chuanyun.lcy@alibaba-inc.com>
2024-01-25 19:12:14 +0800

    fix asi-koord-manager: fix batch root quota resource when no batch resource
    Signed-off-by: chuanyun.lcy <chuanyun.lcy@alibaba-inc.com>

--- pkg/quota-controller/profile/quota_unified.go
+++ pkg/quota-controller/profile/quota_unified.go
@@ -20,12 +20,20 @@ func DecorateResourceByACSQuota(profile *v1alpha1.ElasticQuotaProfile, total cor
 		quantity, ok := total[extension.BatchCPU]
 		if ok {
 			total[corev1.ResourceCPU] = *resource.NewMilliQuantity(quantity.Value(), resource.DecimalSI)
+		} else {
+			total[corev1.ResourceCPU] = *resource.NewMilliQuantity(0, resource.DecimalSI)
 		}
 
 		quantity, ok = total[extension.BatchMemory]
 		if ok {
 			total[corev1.ResourceMemory] = quantity
+		} else {
+			total[corev1.ResourceMemory] = *resource.NewQuantity(0, resource.BinarySI)
 		}
+		delete(total, extension.BatchCPU)
+		delete(total, extension.BatchMemory)
+		delete(total, extension.MidCPU)
+		delete(total, extension.MidMemory)
 	case "standard", "exclusive":
 		// clean batch/mid resource which change frequently, but we don't need it now.
 		delete(total, extension.BatchCPU)
--- pkg/quota-controller/profile/quota_unified_test.go
+++ pkg/quota-controller/profile/quota_unified_test.go
@@ -23,14 +23,21 @@ import (
 )
 
 func createResourceListWithBatch(cpu, mem, batchCPU, batchMemory int64) corev1.ResourceList {
-	return corev1.ResourceList{
+	res := corev1.ResourceList{
 		// use NewMilliQuantity to calculate the runtimeQuota correctly in cpu dimension
 		// when the request is smaller than 1 core.
 		corev1.ResourceCPU:    *resource.NewMilliQuantity(cpu*1000, resource.DecimalSI),
 		corev1.ResourceMemory: *resource.NewQuantity(mem, resource.BinarySI),
-		extension.BatchCPU:    *resource.NewQuantity(batchCPU, resource.DecimalSI),
-		extension.BatchMemory: *resource.NewQuantity(batchMemory, resource.BinarySI),
 	}
+
+	if batchCPU > 0 {
+		res[extension.BatchCPU] = *resource.NewQuantity(batchCPU, resource.DecimalSI)
+	}
+	if batchMemory > 0 {
+		res[extension.BatchMemory] = *resource.NewQuantity(batchMemory, resource.BinarySI)
+	}
+
+	return res
 }
 
 func TestBatchQuotaProfileReconciler_Reconciler_CreateQuota(t *testing.T) {
@@ -39,51 +46,135 @@ func TestBatchQuotaProfileReconciler_Reconciler_CreateQuota(t *testing.T) {
 	quotav1alpha1.AddToScheme(scheme)
 	schedv1alpha1.AddToScheme(scheme)
 
-	nodes := []*corev1.Node{
-		defaultCreateNode("node1", map[string]string{"topology.kubernetes.io/zone": "cn-hangzhou-a"}, createResourceListWithBatch(10, 1000, 2000, 4000)),
-		defaultCreateNode("node2", map[string]string{"topology.kubernetes.io/zone": "cn-hangzhou-a"}, createResourceListWithBatch(10, 1000, 2000, 4000)),
-		defaultCreateNode("node3", map[string]string{"topology.kubernetes.io/zone": "cn-hangzhou-b"}, createResourceListWithBatch(10, 1000, 2000, 4000)),
-	}
-
-	profile := &quotav1alpha1.ElasticQuotaProfile{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "batchProfile",
-			Labels: map[string]string{
-				transformer.LabelInstanceType: transformer.BestEffortInstanceType,
+	testCases := []struct {
+		name        string
+		nodes       []*corev1.Node
+		profile     *quotav1alpha1.ElasticQuotaProfile
+		expectMin   corev1.ResourceList
+		expectTotal corev1.ResourceList
+	}{
+		{
+			name: "standard profile with batch resource",
+			nodes: []*corev1.Node{
+				defaultCreateNode("node1", map[string]string{"topology.kubernetes.io/zone": "cn-hangzhou-a"}, createResourceListWithBatch(10, 1000, 2000, 4000)),
+				defaultCreateNode("node2", map[string]string{"topology.kubernetes.io/zone": "cn-hangzhou-a"}, createResourceListWithBatch(10, 1000, 2000, 4000)),
 			},
-		},
-		Spec: quotav1alpha1.ElasticQuotaProfileSpec{
-			QuotaName: "batchProfile-root",
-			NodeSelector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{"topology.kubernetes.io/zone": "cn-hangzhou-a"},
+			profile: &quotav1alpha1.ElasticQuotaProfile{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "standardProfile",
+					Labels: map[string]string{
+						transformer.LabelInstanceType: "standard",
+					},
+				},
+				Spec: quotav1alpha1.ElasticQuotaProfileSpec{
+					QuotaName: "standardProfile-root",
+					NodeSelector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{"topology.kubernetes.io/zone": "cn-hangzhou-a"},
+					},
+				},
 			},
+			expectMin:   createResourceList(20, 2000),
+			expectTotal: createResourceList(20, 2000),
+		},
+		{
+			name: "standard profile with no batch resource",
+			nodes: []*corev1.Node{
+				defaultCreateNode("node1", map[string]string{"topology.kubernetes.io/zone": "cn-hangzhou-a"}, createResourceList(10, 1000)),
+				defaultCreateNode("node2", map[string]string{"topology.kubernetes.io/zone": "cn-hangzhou-a"}, createResourceList(10, 1000)),
+			},
+			profile: &quotav1alpha1.ElasticQuotaProfile{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "standardProfile",
+					Labels: map[string]string{
+						transformer.LabelInstanceType: "standard",
+					},
+				},
+				Spec: quotav1alpha1.ElasticQuotaProfileSpec{
+					QuotaName: "standardProfile-root",
+					NodeSelector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{"topology.kubernetes.io/zone": "cn-hangzhou-a"},
+					},
+				},
+			},
+			expectMin:   createResourceList(20, 2000),
+			expectTotal: createResourceList(20, 2000),
+		},
+		{
+			name: "batch profile with batch resource",
+			nodes: []*corev1.Node{
+				defaultCreateNode("node1", map[string]string{"topology.kubernetes.io/zone": "cn-hangzhou-a"}, createResourceListWithBatch(10, 1000, 2000, 4000)),
+				defaultCreateNode("node2", map[string]string{"topology.kubernetes.io/zone": "cn-hangzhou-a"}, createResourceListWithBatch(10, 1000, 2000, 4000)),
+			},
+			profile: &quotav1alpha1.ElasticQuotaProfile{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "batchProfile",
+					Labels: map[string]string{
+						transformer.LabelInstanceType: transformer.BestEffortInstanceType,
+					},
+				},
+				Spec: quotav1alpha1.ElasticQuotaProfileSpec{
+					QuotaName: "batchProfile-root",
+					NodeSelector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{"topology.kubernetes.io/zone": "cn-hangzhou-a"},
+					},
+				},
+			},
+			expectMin:   createResourceList(4, 8000),
+			expectTotal: createResourceList(4, 8000),
+		},
+		{
+			name: "batch profile with no batch resource",
+			nodes: []*corev1.Node{
+				defaultCreateNode("node1", map[string]string{"topology.kubernetes.io/zone": "cn-hangzhou-a"}, createResourceList(10, 1000)),
+				defaultCreateNode("node2", map[string]string{"topology.kubernetes.io/zone": "cn-hangzhou-a"}, createResourceList(10, 1000)),
+			},
+			profile: &quotav1alpha1.ElasticQuotaProfile{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "batchProfile",
+					Labels: map[string]string{
+						transformer.LabelInstanceType: transformer.BestEffortInstanceType,
+					},
+				},
+				Spec: quotav1alpha1.ElasticQuotaProfileSpec{
+					QuotaName: "batchProfile-root",
+					NodeSelector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{"topology.kubernetes.io/zone": "cn-hangzhou-a"},
+					},
+				},
+			},
+			expectMin:   createResourceList(0, 0),
+			expectTotal: createResourceList(0, 0),
 		},
 	}
 
-	r := &QuotaProfileReconciler{
-		Client: fake.NewClientBuilder().WithScheme(scheme).Build(),
-		Scheme: scheme,
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			r := &QuotaProfileReconciler{
+				Client: fake.NewClientBuilder().WithScheme(scheme).Build(),
+				Scheme: scheme,
+			}
+			// create node
+			for _, node := range tc.nodes {
+				nodeCopy := node.DeepCopy()
+				err := r.Client.Create(context.TODO(), nodeCopy)
+				assert.NoError(t, err)
+			}
+
+			err := r.Client.Create(context.TODO(), tc.profile)
+			assert.NoError(t, err)
+
+			profileReq := ctrl.Request{NamespacedName: types.NamespacedName{Namespace: tc.profile.Namespace, Name: tc.profile.Name}}
+			r.Reconcile(context.TODO(), profileReq)
+			quota := &schedv1alpha1.ElasticQuota{}
+			err = r.Client.Get(context.TODO(), types.NamespacedName{Namespace: tc.profile.Namespace, Name: tc.profile.Spec.QuotaName}, quota)
+			assert.NoError(t, err)
+
+			total := corev1.ResourceList{}
+			err = json.Unmarshal([]byte(quota.Annotations[extension.AnnotationTotalResource]), &total)
+			assert.NoError(t, err)
+
+			assert.True(t, quotav1.Equals(tc.expectMin, quota.Spec.Min))
+			assert.True(t, quotav1.Equals(tc.expectTotal, total))
+		})
 	}
-	// create node
-	for _, node := range nodes {
-		nodeCopy := node.DeepCopy()
-		err := r.Client.Create(context.TODO(), nodeCopy)
-		assert.NoError(t, err)
-	}
-
-	profileReq := ctrl.Request{NamespacedName: types.NamespacedName{Namespace: profile.Namespace, Name: profile.Name}}
-
-	err := r.Client.Create(context.TODO(), profile)
-	assert.NoError(t, err)
-	r.Reconcile(context.TODO(), profileReq)
-	quota := &schedv1alpha1.ElasticQuota{}
-	err = r.Client.Get(context.TODO(), types.NamespacedName{Namespace: profile.Namespace, Name: profile.Spec.QuotaName}, quota)
-	assert.NoError(t, err)
-
-	total := corev1.ResourceList{}
-	err = json.Unmarshal([]byte(quota.Annotations[extension.AnnotationTotalResource]), &total)
-	assert.NoError(t, err)
-
-	assert.True(t, quotav1.Equals(createResourceList(4, 8000), quota.Spec.Min))
-	assert.True(t, quotav1.Equals(createResourceListWithBatch(4, 8000, 4000, 8000), total))
 }
