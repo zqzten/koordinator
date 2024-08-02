554af5697f5c39ae95c94434524fa5de9cd769b4
zhendi.lwb <zhendi.lwb@antgroup.comgit>
2024-08-02 17:18:40 +0800

    update informer

--- pkg/scheduler/plugins/ack/intelligentscheduler/eventHandler.go
+++ pkg/scheduler/plugins/ack/intelligentscheduler/eventHandler.go
@@ -1,10 +1,13 @@
 package intelligentscheduler
 
 import (
+	"context"
 	"github.com/koordinator-sh/koordinator/pkg/scheduler/plugins/ack/intelligentscheduler/CRDs"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/klog/v2"
 )
@@ -42,16 +45,6 @@ func handleAddOrUpdateNode(node *corev1.Node, ic *intelligentCache) {
 	ic.addOrUpdateNode(node)
 }
 
-func handleAddPod(client dynamic.Interface, pod *corev1.Pod) {
-	err := patchOwnerRefToConfigMap(client, pod)
-	if err != nil {
-		klog.Errorf("Failed to patch owner ref [pod %s/%s] to configmap: %v", pod.Namespace, pod.Name, err)
-		return
-	} else {
-		klog.Infof("Added owner ref [pod %s/%s] to configmap", pod.Namespace, pod.Name)
-	}
-}
-
 func updateNode(ic *intelligentCache, oldNode *corev1.Node, newNode *corev1.Node) {
 	if isIntelligentNode(newNode) {
 		handleAddOrUpdateNode(newNode, ic)
@@ -64,4 +57,44 @@ func updateNode(ic *intelligentCache, oldNode *corev1.Node, newNode *corev1.Node
 
 func deleteNode(ic *intelligentCache, node *corev1.Node) {
 	ic.deleteNode(node)
+}
+
+func handleAddPod(client dynamic.Interface, pod *corev1.Pod) {
+	err := patchOwnerRefToConfigMap(client, pod)
+	if err != nil {
+		klog.Errorf("Failed to patch owner ref [pod %s/%s] to configmap: %v", pod.Namespace, pod.Name, err)
+		return
+	} else {
+		klog.Infof("Added owner ref [pod %s/%s] to configmap", pod.Namespace, pod.Name)
+	}
+}
+
+func handleDeletePod(client dynamic.Interface, pod *corev1.Pod) {
+	podName := pod.Name
+	podNamespace := pod.Namespace
+	name := podName + "/" + podNamespace
+	vgiGvr := schema.GroupVersionResource{
+		Group:    IntelligentGroupName,
+		Version:  IntelligentVersion,
+		Resource: VgiResourceName,
+	}
+	vgiCrs, err := client.Resource(vgiGvr).Namespace(NameSpace).List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		klog.Errorf("Failed to list virtual gpu instance when handle pod deletion: %v", err)
+	}
+	for _, vgiCr := range vgiCrs.Items {
+		klog.Infof("unstructured vgi: [%v]", vgiCr)
+		var vgi CRDs.VirtualGpuInstance
+		er := runtime.DefaultUnstructuredConverter.FromUnstructured(vgiCr.Object, &vgi)
+		if er != nil {
+			klog.Errorf("Failed to convert fetched virtual gpu instance to vgi")
+		}
+		if vgi.Status.Pod == name {
+			deleteErr := client.Resource(vgiGvr).Namespace(NameSpace).Delete(context.Background(), vgi.GetName(), metav1.DeleteOptions{})
+			if deleteErr != nil {
+				klog.Errorf("Failed to delete virtual gpu instance when handle pod deletion: %v", deleteErr)
+			}
+			klog.Infof("Deleted virtual gpu instance %v", vgi.Name)
+		}
+	}
 }
